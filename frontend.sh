9273d7e0144a1ed75962a13033c6a18e87208806
izypsy <reddy.nie11@gmail.com>
2024-03-10 19:47:28 +0530

    reverse proxy conf

--- frontend.sh
+++ frontend.sh
@@ -8,6 +8,10 @@ print_activity "installing_nginx"
 dnf install nginx -y &>>LOG
 status $?
 
+print_activity "reverse proxy config"
+cp proxy.conf /etc/nginx/default.d/proxy.conf &>>LOG
+status $?
+
 prepare_app 
 
 print_activity "start nginx service"
